d1097193a8d1c8095a96de65dd2a7e272dc3af46
deepika47 <37235566+deepika47@users.noreply.github.com>
2019-02-05 10:21:31 +0530

    Create nk.py

---
+++ nk.py
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,k;
+printf("enter the value for n and k:");
+scanf("%d,%d,&n,&k);
+if(n=5)
+{
+c=100%5;
+printf("the average num of integer is",&c);
+}
